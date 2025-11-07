c9c3963663e5b31e6fee921e0fa2b2e8a2677c06
ArchSeraphin <trinity.informatique@gmail.com>
2025-11-07 09:47:10 +0100

    update_formulaire-refphoto

--- single-photo.php
+++ single-photo.php
@@ -5,6 +5,20 @@
  * @package Trinity
  */
 
+$queried_photo = get_queried_object();
+
+if ( $queried_photo instanceof WP_Post && 'photo' === $queried_photo->post_type ) {
+  $query_key        = trinity_get_contact_reference_query_key();
+  $expected_ref     = trinity_get_photo_field_value( $queried_photo->ID, 'reference' );
+  $current_ref      = isset( $_GET[ $query_key ] ) ? sanitize_text_field( wp_unslash( $_GET[ $query_key ] ) ) : '';
+
+  if ( $query_key && $expected_ref && $current_ref !== $expected_ref ) {
+    $redirect_url = add_query_arg( $query_key, $expected_ref, get_permalink( $queried_photo ) );
+    wp_safe_redirect( $redirect_url, 302 );
+    exit;
+  }
+}
+
 get_header();
 
 if ( have_posts() ) {
